98104acde8467f9163e2591c36270719b57d6c67
CeciliaChen <cecilia@pdotf.com>
2023-07-27 20:27:02 -0400

    Initial Commit

---
+++ APICallSwiftUICombine/APICallSwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallSwiftUICombineApp.swift
+//  APICallSwiftUICombine
+//
+//  Created by Cecilia Chen on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct APICallSwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
